aef4885daa6d9838418388aeee4b6edbaf8d5dec
Ian Springer <ian.springer@salesforce.com>
2024-03-21 15:17:46 -0400

    remove old XmlDoclet class, as it no longer compiles under jdk21

--- src/main/java/com/github/markusbernhardt/xmldoclet/XmlDoclet.java
+++ src/main/java/com/github/markusbernhardt/xmldoclet/XmlDoclet.java
@@ -1,263 +1,194 @@
 package com.github.markusbernhardt.xmldoclet;
 
+import com.github.markusbernhardt.xmldoclet.xjc.Annotation;
+import com.github.markusbernhardt.xmldoclet.xjc.Class;
+import com.github.markusbernhardt.xmldoclet.xjc.Enum;
+import com.github.markusbernhardt.xmldoclet.xjc.Interface;
+import com.github.markusbernhardt.xmldoclet.xjc.Package;
+import com.github.markusbernhardt.xmldoclet.xjc.Root;
 import java.io.BufferedOutputStream;
+import java.io.CharArrayWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
+import java.io.OutputStream;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.ListIterator;
-
+import java.util.Locale;
+import java.util.Set;
+import java.util.function.Consumer;
+import javax.lang.model.SourceVersion;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import jdk.javadoc.doclet.Doclet;
+import jdk.javadoc.doclet.DocletEnvironment;
+import jdk.javadoc.doclet.Reporter;
 
-import org.apache.commons.cli.BasicParser;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.OptionBuilder;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import com.github.markusbernhardt.xmldoclet.xjc.Root;
-import com.sun.javadoc.DocErrorReporter;
-import com.sun.javadoc.LanguageVersion;
-import com.sun.javadoc.RootDoc;
+public class XmlDoclet implements Doclet {
 
-/**
- * Doclet class.
- * 
- * @author markus
- */
-public class XmlDoclet {
+    /**
+     * For tests.
+     */
+    public static Root root;
 
-	private final static Logger log = LoggerFactory.getLogger(Parser.class);
+    private String directory;
+    private String encoding;
+    private boolean dryrun;
+    private String filename;
 
-	/**
-	 * The parsed object model. Used in unit tests.
-	 */
-	public static Root root;
+    @Override
+    public void init(Locale locale, Reporter reporter) {
+    }
 
-	/**
-	 * The Options instance to parse command line strings.
-	 */
-	public final static Options options;
+    @Override
+    public String getName() {
+        return getClass().getSimpleName();
+    }
 
-	static {
-		options = new Options();
+    @Override
+    public Set<? extends Option> getSupportedOptions() {
+        return new LinkedHashSet<>(Arrays.asList(
+                new SingleArgumentOption(
+                        Arrays.asList("-d"),
+                        "directory",
+                        "Destination directory for output file.\nDefault: .",
+                        argument -> this.directory = argument
+                ),
+                new SingleArgumentOption(
+                        Arrays.asList("-docencoding"),
+                        "encoding",
+                        "Encoding of the output file.\nDefault: UTF8",
+                        argument -> this.encoding = argument
+                ),
+                new FlagOption(
+                        Arrays.asList("-dryrun"),
+                        "Parse javadoc, but don't write output file.\nDefault: false",
+                        () -> this.dryrun = true
+                ),
+                new SingleArgumentOption(
+                        Arrays.asList("-filename"),
+                        "filename",
+                        "Name of the output file.\nDefault: javadoc.xml",
+                        argument -> this.filename = argument
+                )
+        ));
+    }
 
-		OptionBuilder.withArgName("directory");
-		OptionBuilder.isRequired(false);
-		OptionBuilder.hasArg();
-		OptionBuilder.withDescription("Destination directory for output file.\nDefault: .");
-		options.addOption(OptionBuilder.create("d"));
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latest();
+    }
 
-		OptionBuilder.withArgName("encoding");
-		OptionBuilder.isRequired(false);
-		OptionBuilder.hasArg();
-		OptionBuilder.withDescription("Encoding of the output file.\nDefault: UTF8");
-		options.addOption(OptionBuilder.create("docencoding"));
+    @Override
+    public boolean run(DocletEnvironment environment) {
+        final Root xmlRoot = new JavadocTransformer(environment).transform();
+        root = xmlRoot;
+        save(xmlRoot);
+        return true;
+    }
 
-		OptionBuilder.withArgName("dryrun");
-		OptionBuilder.isRequired(false);
-		OptionBuilder.hasArgs(0);
-		OptionBuilder.withDescription("Parse javadoc, but don't write output file.\nDefault: false");
-		options.addOption(OptionBuilder.create("dryrun"));
+    private void save(Root xmlRoot) {
+        ClassLoader originalClassLoader = null;
+        try {
+            originalClassLoader = Thread.currentThread().getContextClassLoader();
+            Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
+            if (dryrun) {
+                return;
+            }
+            sort(xmlRoot);
+            final JAXBContext context = JAXBContext.newInstance(Root.class);
+            final CharArrayWriter writer = new CharArrayWriter();
+            final Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            if (encoding != null) {
+                marshaller.setProperty(Marshaller.JAXB_ENCODING, encoding);
+            }
+            final String name = (directory != null ? directory + File.separator : "") + (filename != null ? filename : "javadoc.xml");
+            try (OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(name))) {
+                marshaller.marshal(xmlRoot, outputStream);
+            }
+            marshaller.marshal(xmlRoot,  writer);
+        } catch (JAXBException | IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (originalClassLoader != null) {
+                Thread.currentThread().setContextClassLoader(originalClassLoader);
+            }
+        }
+    }
 
-		OptionBuilder.withArgName("filename");
-		OptionBuilder.isRequired(false);
-		OptionBuilder.hasArg();
-		OptionBuilder.withDescription("Name of the output file.\nDefault: javadoc.xml");
-		options.addOption(OptionBuilder.create("filename"));
-	}
+    public static void sort(Root xmlRoot) {
+        xmlRoot.getPackage().sort(Comparator.comparing(Package::getName));
+        for (Package pkg : xmlRoot.getPackage()) {
+            pkg.getAnnotation().sort(Comparator.comparing(Annotation::getQualified));
+            pkg.getEnum().sort(Comparator.comparing(Enum::getQualified));
+            pkg.getInterface().sort(Comparator.comparing(Interface::getQualified));
+            pkg.getClazz().sort(Comparator.comparing(Class::getQualified));
+        }
+    }
 
-	/**
-	 * Check for doclet-added options. Returns the number of arguments you must
-	 * specify on the command line for the given option. For example, "-d docs"
-	 * would return 2.
-	 * <P>
-	 * This method is required if the doclet contains any options. If this
-	 * method is missing, Javadoc will print an invalid flag error for every
-	 * option.
-	 * 
-	 * @see com.sun.javadoc.Doclet#optionLength(String)
-	 * 
-	 * @param optionName
-	 *            The name of the option.
-	 * @return number of arguments on the command line for an option including
-	 *         the option name itself. Zero return means option not known.
-	 *         Negative value means error occurred.
-	 */
-	public static int optionLength(String optionName) {
-		Option option = options.getOption(optionName);
-		if (option == null) {
-			return 0;
-		}
-		return option.getArgs() + 1;
-	}
+    private static class FlagOption extends StandardOption {
+        public FlagOption(List<String> names, String description, Runnable processor) {
+            super(names, null, description, 0, arguments -> processor.run());
+        }
+    }
 
-	/**
-	 * Check that options have the correct arguments.
-	 * <P>
-	 * This method is not required, but is recommended, as every option will be
-	 * considered valid if this method is not present. It will default
-	 * gracefully (to true) if absent.
-	 * <P>
-	 * Printing option related error messages (using the provided
-	 * DocErrorReporter) is the responsibility of this method.
-	 * 
-	 * @see com.sun.javadoc.Doclet#validOptions(String[][], DocErrorReporter)
-	 * 
-	 * @param optionsArrayArray
-	 *            The two dimensional array of options.
-	 * @param reporter
-	 *            The error reporter.
-	 * 
-	 * @return <code>true</code> if the options are valid.
-	 */
-	public static boolean validOptions(String optionsArrayArray[][], DocErrorReporter reporter) {
-		return null != parseCommandLine(optionsArrayArray);
-	}
+    private static class SingleArgumentOption extends StandardOption {
+        public SingleArgumentOption(List<String> names, String parameter, String description, Consumer<String> processor) {
+            super(names, parameter, description, 1, arguments -> processor.accept(arguments.get(0)));
+        }
+    }
 
-	/**
-	 * Processes the JavaDoc documentation.
-	 * <p>
-	 * This method is required for all doclets.
-	 * 
-	 * @see com.sun.javadoc.Doclet#start(RootDoc)
-	 * 
-	 * @param rootDoc
-	 *            The root of the documentation tree.
-	 * 
-	 * @return <code>true</code> if processing was successful.
-	 */
-	public static boolean start(RootDoc rootDoc) {
-		CommandLine commandLine = parseCommandLine(rootDoc.options());
-		Parser parser = new Parser();
-		root = parser.parseRootDoc(rootDoc);
-		save(commandLine, root);
-		return true;
-	}
+    private static class StandardOption implements Option {
 
-	/**
-	 * Save XML object model to a file via JAXB.
-	 * 
-	 * @param commandLine
-	 *            the parsed command line arguments
-	 * @param root
-	 *            the document root
-	 */
-	public static void save(CommandLine commandLine, Root root) {
-		if (commandLine.hasOption("dryrun")) {
-			return;
-		}
-		// for easier comparison between original and new version
-		NewXmlDoclet.sort(root);
+        private final List<String> names;
+        private final String parameters;
+        private final String description;
+        private final int argumentCount;
+        private final Consumer<List<String>> processor;
 
-		FileOutputStream fileOutputStream = null;
-		BufferedOutputStream bufferedOutputStream = null;
-		try {
-			JAXBContext contextObj = JAXBContext.newInstance(Root.class);
+        public StandardOption(List<String> names, String parameters, String description, int argumentCount, Consumer<List<String>> processor) {
+            this.names = names;
+            this.parameters = parameters;
+            this.description = description;
+            this.argumentCount = argumentCount;
+            this.processor = processor;
+        }
 
-			Marshaller marshaller = contextObj.createMarshaller();
-			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-			if (commandLine.hasOption("docencoding")) {
-				marshaller.setProperty(Marshaller.JAXB_ENCODING, commandLine.getOptionValue("docencoding"));
-			}
+        @Override
+        public int getArgumentCount() {
+            return argumentCount;
+        }
 
-			String filename = "javadoc.xml";
-			if (commandLine.hasOption("filename")) {
-				filename = commandLine.getOptionValue("filename");
-			}
-			if (commandLine.hasOption("d")) {
-				filename = commandLine.getOptionValue("d") + File.separator + filename;
-			}
+        @Override
+        public String getDescription() {
+            return description;
+        }
 
-			fileOutputStream = new FileOutputStream(filename);
-			bufferedOutputStream = new BufferedOutputStream(fileOutputStream, 1024 * 1024);
+        @Override
+        public Kind getKind() {
+            return Kind.STANDARD;
+        }
 
-			marshaller.marshal(root, bufferedOutputStream);
-			bufferedOutputStream.flush();
-			fileOutputStream.flush();
+        @Override
+        public List<String> getNames() {
+            return names;
+        }
 
-		} catch (JAXBException e) {
-			log.error(e.getMessage(), e);
-		} catch (FileNotFoundException e) {
-			log.error(e.getMessage(), e);
-		} catch (IOException e) {
-			log.error(e.getMessage(), e);
-		} finally {
-			try {
-				if (bufferedOutputStream != null) {
-					bufferedOutputStream.close();
-				}
-				if (fileOutputStream != null) {
-					fileOutputStream.close();
-				}
-			} catch (IOException e) {
-				log.error(e.getMessage(), e);
-			}
-		}
-	}
+        @Override
+        public String getParameters() {
+            return parameters;
+        }
 
-	/**
-	 * Return the version of the Java Programming Language supported by this
-	 * doclet.
-	 * <p>
-	 * This method is required by any doclet supporting a language version newer
-	 * than 1.1.
-	 * <p>
-	 * This Doclet supports Java 5.
-	 * 
-	 * @see com.sun.javadoc.Doclet#languageVersion()
-	 * 
-	 * @return LanguageVersion#JAVA_1_5
-	 */
-	public static LanguageVersion languageVersion() {
-		return LanguageVersion.JAVA_1_5;
-	}
+        @Override
+        public boolean process(String option, List<String> arguments) {
+            processor.accept(arguments);
+            return true;
+        }
+    }
 
-	/**
-	 * Parse the given options.
-	 * 
-	 * @param optionsArrayArray
-	 *            The two dimensional array of options.
-	 * @return the parsed command line arguments.
-	 */
-	public static CommandLine parseCommandLine(String[][] optionsArrayArray) {
-		try {
-			List<String> argumentList = new ArrayList<String>();
-			for (String[] optionsArray : optionsArrayArray) {
-				argumentList.addAll(Arrays.asList(optionsArray));
-			}
-
-			CommandLineParser commandLineParser = new BasicParser() {
-				@Override
-				protected void processOption(final String arg, @SuppressWarnings("rawtypes") final ListIterator iter)
-						throws ParseException {
-					boolean hasOption = getOptions().hasOption(arg);
-					if (hasOption) {
-						super.processOption(arg, iter);
-					}
-				}
-			};
-			CommandLine commandLine = commandLineParser.parse(options, argumentList.toArray(new String[] {}));
-			return commandLine;
-		} catch (ParseException e) {
-			LoggingOutputStream loggingOutputStream = new LoggingOutputStream(log, LoggingLevelEnum.INFO);
-			PrintWriter printWriter = new PrintWriter(loggingOutputStream);
-
-			HelpFormatter helpFormatter = new HelpFormatter();
-			helpFormatter.printHelp(printWriter, 74, "javadoc -doclet " + XmlDoclet.class.getName() + " [options]",
-					null, options, 1, 3, null, false);
-			return null;
-		}
-	}
 }
